ff57ea139e70e6c99fefd5c44671a65013dbf694
jonatansouza <jonatangd.souza@gmail.com>
2017-01-06 17:52:20 -0200

    separated file for socket client

---
+++ public/js/controller-sock.js
@@ -0,0 +1,81 @@
+function clearChat() {
+    $('#chat').empty();
+    $('#message').focus();
+}
+$(function() {
+    $("#message").keyup(function(event) {
+        if (event.keyCode == 13) {
+            $("#sendMessage").click();
+        }
+    });
+
+    moment.locale('br');
+    var socket = io.connect();
+    var $messageForm = $('#messageForm');
+    var $message = $('#message');
+    var $chat = $('#chat');
+    var $messageArea = $('#messageArea');
+    var $userFormArea = $('#userFormArea');
+    var $userForm = $('#userForm');
+    var $users = $('#users');
+    var $username = $('#userSession').val();
+
+    $messageForm.submit(function(e) {
+
+        e.preventDefault();
+
+        (function($) {
+            $.sanitize = function(input) {
+                var output = input.replace(/<script[^>]*?>.*?<\/script>/gi, '').
+                replace(/<[\/\!]*?[^<>]*?>/gi, '').
+                replace(/<style[^>]*?>.*?<\/style>/gi, '').
+                replace(/<![\s\S]*?--[ \t\n\r]*>/gi, '');
+                return output;
+            };
+        })(jQuery);
+
+        var m = $.sanitize($message.val());
+        if (m) {
+            socket.emit('send message', m);
+            $message.val('');
+            $message.focus();
+        } else {
+            $message.val('');
+            $message.focus();
+        }
+    });
+
+    socket.on('new message', function(data) {
+        if ($(".usernames").last().text() === data.user) {
+            $chat.children('.well').last().append('<p>' + data.msg + ' <span class="pull-right"><small>' + moment().calendar() + '</smal></span ></p>');
+        } else {
+            $chat.append('<div class="well"><p><strong class="text-info"><span class="usernames">' + data.user + '</span> Says: </strong></p><p>' + data.msg + ' <span class="pull-right"><small>' + moment().calendar() +
+                '</smal></span ></p></div>');
+        }
+
+    });
+
+    socket.emit('new username', $username, function(data) {
+        console.log(data+ "added");
+    });
+
+    socket.on('new user in', function(data){
+      $chat.append('<div class="well well-success"><strong class="text-success"><span class="usernameStatus">' + data + '</span></strong> join in chat  <span class="pull-right"><small>' + moment().calendar() +
+          '</smal></span ></div>');
+    });
+
+    socket.on('user off', function(data) {
+        if (data != $username) {
+            $chat.append('<div class="well well-warning"><strong class="text-warning"><span class="usernameStatus">' + data + '</span></strong> is offline now <span class="pull-right"><small>' + moment().calendar() +
+                '</smal></span ></div>')
+        }
+    });
+
+    socket.on('get users', function(data) {
+        var html = '';
+        for (i = 0; i < data.length; i++) {
+            html += '<li class="list-group-item">' + data[i] + '</i>';
+        }
+        $users.html(html);
+    });
+});
